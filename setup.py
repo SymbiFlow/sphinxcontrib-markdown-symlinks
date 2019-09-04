1168eb75a24d5792493e403f76c9447d2b8a0776
Alessandro Comodi <acomodi@antmicro.com>
2019-09-04 14:49:38 +0200

    add markdown_code_symlinks code
    Signed-off-by: Alessandro Comodi <acomodi@antmicro.com>

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="markdown_code_symlinks",
+    version="0.0.1",
+    author="SymbiFlow Authors",
+    author_email="symbiflow@lists.librecores.org",
+    description="Python library to solve cross-reference links \
+            when building sphinx documentation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SymbiFlow/markdown-code-symlinks",
+    packages=setuptools.find_packages(),
+    install_requires=['docutils', 'sphinx', 'recommonmark'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: ISC License",
+        "Operating System :: OS Independent",
+    ],
+)
